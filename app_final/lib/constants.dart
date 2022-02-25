3db8af7759dad040c4f139163f0d12f6e38fd657
Jaider Ivan Vera Jaimes <jsjlds66@gmail.com>
2022-02-24 19:59:08 -0500

    Avance

--- app_final/lib/constants.dart
+++
@@ -1,6 +0,0 @@
-import 'package:flutter/material.dart';
-import 'package:flutter/src/material/colors.dart';
-
-const kTextColor = Colors.black;
-const kTextLightColor = Color(0xFFACACAC);
-const kDefaultPaddin = 20.0;
